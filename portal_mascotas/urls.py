6af952eee8df223e74a361a5ba20c5bdec717c4a
Meliodas1231 <aronbarrientos98@gmail.com>
2025-09-19 21:12:36 -0300

    chore: actualizaciones locales

--- portal_mascotas/urls.py
+++ portal_mascotas/urls.py
@@ -1,17 +1,18 @@
 # portal_mascotas/urls.py
 from django.contrib import admin
 from django.urls import path, include
+from portal_mascotas import views
 from django.conf import settings
 from django.conf.urls.static import static
-from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
     path('admin/', admin.site.urls),
-    path('accounts/', include('login.urls')),
-    path('blog/', include('blog.urls')),
-    path(
-        "solicitudes/",
-        include(("solicitud_adopcion.urls", "solicitud_adopcion"), namespace="solicitud_adopcion"),
-    ),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('', views.home, name='home'),
+    path('accounts/', include(('login.urls', 'login'), namespace='login')),
+    path('solicitudes/', include(('solicitud_adopcion.urls', 'solicitud_adopcion'), namespace='solicitud_adopcion')),
+    path('blog/', include(('blog.urls', 'blog'), namespace='blog')),
+]
+
+# Servir media en desarrollo
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
